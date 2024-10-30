72e6589a5eeab2050e69a77589f1bae81404f805
WalterWendji <rahaman.dongmepi@stud.th-owl.de>
2024-10-30 21:25:02 +0100

    microsoft solution file added temporary game name added

--- CofStory.cs
+++ CofStory.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Gruppe_2;
 
-public class Game1 : Game
+public class CofStory : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    public Game1()
+    public CofStory()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
