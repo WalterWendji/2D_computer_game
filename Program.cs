93faff03904e8001ce48423549da8bd0e9094540
WalterWendji <rahaman.dongmepi@stud.th-owl.de>
2024-10-31 12:37:22 +0100

    naming error fixed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿using var game = new Gruppe_2.Game1();
+﻿using var game = new Gruppe_2.CofStory();
 game.Run();
--- obj/Debug/net8.0/Gruppe-2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gruppe-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gruppe-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6725ec3b6823838835227fd1fbf48f3b6205a7cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+970573aa8b8eb8fb9b93636d502a5ac5dd27901e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gruppe-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gruppe-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
