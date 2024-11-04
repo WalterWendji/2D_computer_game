d6df94190a743ab709bcba9f403ba89adb068fe1
WalterWendji <rahaman.dongmepi@stud.th-owl.de>
2024-11-04 21:15:32 +0100

    background menu added. Tried to set the menu button in the middle of the screen whatever the width of the device is.

--- CODE_OF_STORY/Scenes/MenuScene.cs
+++ CODE_OF_STORY/Scenes/MenuScene.cs
@@ -16,6 +16,8 @@ internal class MenuScene : Component
     private Texture2D[] btns;
     private Texture2D[] btnsColored;
     private Rectangle[] btnRects;
+    private Texture2D menuBackground;
+    private Rectangle BgRectangle;
     private MouseState currentMouseState, oldMouseState;
     private Rectangle currentMouseStateRectangle;
 
@@ -24,20 +26,24 @@ internal class MenuScene : Component
         btns = new Texture2D[buttonNames.Length];
         btnsColored = new Texture2D[buttonNamesColored.Length];
         btnRects = new Rectangle[buttonNames.Length];
+        BgRectangle = new Rectangle();
     }
+
     internal override void LoadContent(ContentManager Content)
     {
+       menuBackground = Content.Load<Texture2D>("Items/bg_desertM");
+       BgRectangle = new Rectangle (0, 0, menuBackground.Width*6, menuBackground.Height*3);
         const int INCREMENT_VALUE = 100;
         for (int i = 0; i < btns.Length; i++)
         {
             btns[i] = Content.Load<Texture2D>($"Buttons/{buttonNames[i]}");
-            btnRects[i] = new Rectangle(500, 80 + (INCREMENT_VALUE * i), btns[i].Width / 4, btns[i].Height / 4);
+            btnRects[i] = new Rectangle(700, 300 + (INCREMENT_VALUE * i), btns[i].Width / 4, btns[i].Height / 4);
             
         }
         for (int i = 0; i<btnsColored.Length; i++)
         {
             btnsColored[i] = Content.Load<Texture2D>($"Buttons/ColoredButtons/{buttonNamesColored[i]}");
-            btnRects[i] = new Rectangle(500, 80 + (INCREMENT_VALUE * i), btnsColored[i].Width / 4, btnsColored[i].Height / 4);
+            btnRects[i] = new Rectangle(700, 300 + (INCREMENT_VALUE * i), btnsColored[i].Width / 4, btnsColored[i].Height / 4);
         }
     }
 
@@ -53,11 +59,29 @@ internal class MenuScene : Component
             Data.Exit = true;
     }
 
+    public void CalculateButtonPositons ()
+    {
+        int buttonHeight = btns[0]?.Height?? 0;
+        int totalHeight = buttonHeight * btns.Length;
+       // int screenWidth = GraphicsDevice.Viewport.Width;
+        //int screenHeight = GraphicsDevice.Viewport.Height;
+
+        /* for (int i = 0; i < btns.Length; i++)
+        {
+            int x = (screenWidth - btns[i].Width) / 2;
+            int y = (screenHeight - totalHeight) / 2 + i * buttonHeight;
+            btnPositions[i] = new Vector2(x, y);
+        } */
+    }
+
     internal override void Draw(SpriteBatch spriteBatch)
     {
+        spriteBatch.Draw(menuBackground, BgRectangle, Color.White);
         for (int i=0; i<btns.Length; i++)
         {
             spriteBatch.Draw(btns[i], btnRects[i], Color.White);
+            
+            //Hovering effect to the button.
             if(currentMouseStateRectangle.Intersects(btnRects[i]))
             {
                 spriteBatch.Draw(btnsColored[i], btnRects[i], Color.White);
