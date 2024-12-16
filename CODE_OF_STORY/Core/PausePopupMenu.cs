eb3f86a2e945c22c02e86e0c946a86a9200c84e0
WalterWendji <rahaman.dongmepi@stud.th-owl.de>
2024-12-16 09:41:21 +0100

    useless commits in the pop up menu class is deleted. aser3wrerewrerewr

--- CODE_OF_STORY/Core/PausePopupMenu.cs
+++ CODE_OF_STORY/Core/PausePopupMenu.cs
@@ -119,37 +119,6 @@ internal class PausePopupMenu : Component
                 Data.currentState = Data.Scenes.Gateways;
         }
 
-        /* if (currentMouseState.LeftButton == ButtonState.Pressed && currentMouseStateRectangle.Intersects(btnRects[0]))
-        {
-            Data.currentGameState = Data.GameState.Playing;
-            StoneAge.popUpMenuTriggerd = false;
-        }
-        else if (currentMouseState.LeftButton == ButtonState.Pressed && currentMouseStateRectangle.Intersects(btnRects[1]))
-        {
-            isClicked = true;
-            currentGameState = GameState.Playing;
-            Data.currentState = Data.Scenes.StoneAge;
-        }
-        else if (currentMouseState.LeftButton == ButtonState.Pressed && currentMouseStateRectangle.Intersects(btnRects[2]))
-        {
-            Data.currentState = Data.Scenes.Menu;
-            StoneAge.popUpMenuTriggerd = false;
-        }
-        else if (currentMouseState.LeftButton == ButtonState.Pressed && currentMouseStateRectangle.Intersects(btnRects[3]))
-        {
-            Data.currentState = Data.Scenes.Settings;
-            StoneAge.popUpMenuTriggerd = false;
-        }
-        else if (currentMouseState.LeftButton == ButtonState.Pressed && currentMouseStateRectangle.Intersects(btnRects[4]))
-            Data.Exit = true;
-        else if (currentMouseState.LeftButton == ButtonState.Pressed && currentMouseStateRectangle.Intersects(homeButtonRect))
-        {
-            Data.currentState = Data.Scenes.Gateways;
-            StoneAge.popUpMenuTriggerd = false;
-        } */
-
-        //Stop the game: StoneAge.popUpMenuTriggerd = false; Don't delete pls!
-
     }
     internal override void Draw(SpriteBatch spriteBatch)
     {
